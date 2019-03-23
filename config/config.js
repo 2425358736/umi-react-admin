e5ccb1ab8bfb68b49055b1eaba8f282d22d5d27c
2425358736 <2425358736@qq.com>
2019-03-23 10:01:35 +0800

    性别

--- config/config.js
+++ config/config.js
@@ -98,11 +98,11 @@ export default {
         },
         {
           path: '/sys/sendRecord',
-          component: './userCenter/SendRecord/Index',
+          component: './userCenter/SendRecord/index',
         },
         {
           path: '/sys/insideMail',
-          component: './userCenter/InsideMail/Index',
+          component: './userCenter/InsideMail/index',
         },
         {
           path: '/sys/sysDict',
