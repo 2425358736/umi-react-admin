8b23c8fa39426217e546b102362c2cf0d9f55b38
zhangzuorong <994255496@qq.com>
2019-05-29 22:24:23 +0800

    zzr

---
+++ src/services/FirstPartyInterface.js
@@ -0,0 +1,6 @@
+export const FIRST_PARTY_ADD = '/firstParty/addPartyFactory'; // 添加甲方工厂
+export const FIRST_PARTY_UPDATE = '/firstParty/updatePartyFactory'; // 编辑甲方工厂
+export const FIRST_PARTY_DEL = '/firstParty/delPartyFactory'; // 删除甲方工厂
+export const FIRST_PARTY_LIST = '/firstParty/getPartyFactoryList'; // 获取工厂列表
+export const FIRST_PARTY_MSG = '/firstParty/getPartyFactoryMsg'; // 获取工厂详情
+export const FIRST_PARTY_HEAD = '/firstParty/getFactoryCount'; // 获取头部信息
