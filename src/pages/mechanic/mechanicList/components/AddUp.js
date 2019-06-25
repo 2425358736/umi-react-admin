39e950cd9b3af1bd4ebf3dc1aff0a7b4bef3be73
zhangzuorong <994255496@qq.com>
2019-06-25 20:11:07 +0800

    劳务

---
+++ src/pages/mechanic/mechanicList/components/AddUp.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Spin, Button, Input, Form, notification } from 'antd';
+import { postRequest, jsonString } from '@/utils/api';
+import { MECHANIC_MSG, MECHANIC_UPDATE } from '@/services/FirstPartyInterface';
+
+const styles = require('./AddUp.less');
+
+const FormItem = Form.Item;
+class AddUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonLoading: false,
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.initialization();
+  }
+
+  initialization = async () => {
+    this.props.form.resetFields();
+    if (this.props.id > 0) {
+      this.setState({ loading: true });
+      const data = await postRequest(`${MECHANIC_MSG}/${this.props.id}`);
+      this.props.form.setFieldsValue({
+        jobRemarks: data.data.jobRemarks,
+      });
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  handleSubmit = async () => {
+    let adopt = false;
+    this.props.form.validateFields(err => {
+      adopt = !err;
+    });
+    if (adopt) {
+      this.setState({
+        buttonLoading: true,
+      });
+      const json = this.props.form.getFieldsValue();
+      jsonString(json);
+      json.id = this.props.id;
+      const data = await postRequest(MECHANIC_UPDATE, json);
+      this.setState({
+        buttonLoading: false,
+      });
+      if (data.status === 200) {
+        notification.success({ message: data.msg });
+        this.props.callback(true);
+      } else {
+        notification.error({ message: data.msg, description: data.subMsg });
+      }
+    }
+  };
+
+  handleCancel = () => {
+    this.props.callback(false);
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Spin spinning={this.state.loading}>
+        <div className={styles.formWrap}>
+          <Form layout="horizontal">
+            <div className={styles.titleDom}>
+              <span />
+              <span>职务备注</span>
+            </div>
+            <div className={styles.rowDom}>
+              <div className={styles.colDom}>
+                <FormItem label="职务备注" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }}>
+                  {getFieldDecorator('jobRemarks', {
+                    rules: [
+                      {
+                        required: true,
+                        message: '请输入职务备注',
+                      },
+                    ],
+                  })(<Input placeholder="请输入职务备注" />)}
+                </FormItem>
+              </div>
+            </div>
+            {/* 分割线 */}
+          </Form>
+          <div className={styles.btnGroup}>
+            <Button
+              className={styles.submitBtn}
+              loading={this.state.buttonLoading}
+              onClick={this.handleSubmit}
+              type="primary"
+            >
+              提交
+            </Button>
+            <Button onClick={this.handleCancel} className={styles.cancelBtn}>
+              取消
+            </Button>
+          </div>
+        </div>
+      </Spin>
+    );
+  }
+}
+
+const AddUpComponent = Form.create()(AddUp);
+
+export default AddUpComponent;
