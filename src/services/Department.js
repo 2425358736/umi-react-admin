e6198ab1ed4591dcb7d9c36c14f77b06c393e2ce
zhangzuorong <994255496@qq.com>
2019-07-04 11:40:42 +0800

    部门

---
+++ src/services/Department.js
@@ -0,0 +1,7 @@
+export const DEPARTMENT_TREE = '/department/departmentListAllTree'; // 部门树结构
+
+export const DEL_DEPARTMENT = '/department/delDepartment'; // 删除部门
+
+export const ADD_DEPARTMENT = '/department/addDepartment'; // 添加部门
+
+export const UP_DEPARTMENT = '/department/updateDepartment'; // 修改部门
